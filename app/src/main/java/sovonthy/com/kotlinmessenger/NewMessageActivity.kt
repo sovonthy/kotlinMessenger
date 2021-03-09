459dfa7819329265fb1b5924725fba1f1a00afcc
sovonthy <sovonthy.leng069@gmail.com>
2021-03-09 17:04:27 +0700

    recyclerview message

--- app/src/main/java/sovonthy/com/kotlinmessenger/NewMessageActivity.kt
+++ app/src/main/java/sovonthy/com/kotlinmessenger/NewMessageActivity.kt
@@ -2,10 +2,35 @@ package sovonthy.com.kotlinmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
+
+        supportActionBar?.title = "Select User"
+
+        val adapter = GroupAdapter<ViewHolder>()
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+
+        recyclerview_new_message.adapter = adapter
+
+    }
+}
+
+class UserItem: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+       return R.layout.user_row_message
     }
 }
